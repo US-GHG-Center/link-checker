8b23c824ab14af3d58b93bc11feda8c6a7a4e290
Abdelhak Marouane <am0089@uah.com>
2023-12-19 19:04:34 -0600

    Fix crawler and ouput report as index.html

--- link_checker_test/index.js
+++ link_checker_test/index.js
@@ -14,6 +14,7 @@ const crawled = [];
 let discoveredLinks = [];
 let discoveredImages = [];
 let all_images = []
+let checked_links = []
 let external_links = []
 let failed_urls = []
 
@@ -58,7 +59,8 @@ const generate_index_html = (url_errors) => {
     <h1>Error 500 - Server Error</h1>
     <p>List of URLs with errors:</p>
     <ul>
-      ${url_errors.map(url => `<li>${url}</li>`).join('')}
+    ${url_errors.map(({ url, error }) => `<li><a href=${url}>${url}</a> - Error message: <span style="color: red">${error}</span></li>`).join('')}
+      
     </ul>
 `;
     
@@ -118,26 +120,24 @@ test('Crawl for bad URIs', async () => {
     const links = [...new Set(discoveredLinks)];
     discoveredLinks = [];
     for (let i = 0; i < links.length; i++) {
-      console.log(links[i], external_links)
 
-      if (links[i].indexOf("https://") == 0) {
-        external_links.push(links[i])
+      if (links[i].indexOf("https://") == 0 && !checked_links.includes(links[i]) ) {
+        checked_links.push(links[i])
+        
 
       await fetch_url(links[i]).then((response) => {
      
-            expect(response.status, `Expected a successful HTTP response for external URL ${links[i]}`).toBeLessThan(404);
-            console.log(`External URL ${links[i]}: ✅`);
+            expect(response.status, `Expected a successful HTTP response for URL ${links[i]}`).toBeLessThan(404);
+            console.log(`${links[i]}: ✅`);
 
         })
         .catch((error) => {
             // Handle the error (e.g., log it) without rethrowing it
-            console.log(`External Failed ${links[i]}: ❌\nRaison: ${JSON.stringify(error)}`);
-            failed_urls.push(links[i]);
+            console.log(`Failed ${links[i]}: ❌\nRaison: ${error.message}`);
+            failed_urls.push({url:links[i], error: error.message});
           }
     )
     
-
-
     }
       
       if (!crawled.includes(links[i]) && links[i].indexOf(baseUrl + startPath) == 0) {
@@ -151,17 +151,15 @@ test('Crawl for bad URIs', async () => {
         .catch((error) =>  {
                 // Handle the error (e.g., log it) without rethrowing it
                 console.log(`URL Failed ${links[i]}: ❌\nRaison: ${JSON.stringify(error)}`);
-                failed_urls.push(links[i])
+              
+                failed_urls.push({url:links[i], error: error.message});
               }
     
         )
 
-    
-        
-
 
         await crawl(links[i]);
-        console.log(`${links[i]}: ✅`)
+
       }
 
     }
@@ -169,13 +167,15 @@ test('Crawl for bad URIs', async () => {
 
   // Kick off the crawl
   await crawl(baseUrl + startPath);
-  console.log('Crawl checked', crawled.length);
-  console.log(`Failed URLS: ${failed_urls}`)
 
-  generate_index_html(failed_urls);
 
   // Close the browser
   await browser.close();
+
+  console.log('Crawl checked', crawled.length);
+  console.log(`Failed URLS: ${JSON.stringify(failed_urls)}`)
+
+  generate_index_html(failed_urls);
 });
 
 function handleHtmlDocument(text) {
@@ -203,7 +203,8 @@ async function crawlImages() {
         })
         .catch((err) => {
             console.log(`IMAGE URL Failed ${images[i]}: ❌\nRaison: ${JSON.stringify(err)}`);
-            failed_urls.push(images[i])
+            
+            failed_urls.push({url:images[i], error: err.message});
         })
 
      
@@ -223,14 +224,17 @@ function addUri(collection, uri) {
 
   if (uri.substring(0, 1) == '/') {
     collection.push(baseUrl + uri);
+    return
   }
 
   if (uri.indexOf(baseUrl) == 0) {
     collection.push(uri.split('#')[0]);
+    return
   }
   if (uri.indexOf("https://") == 0 && !external_links.includes(uri)) {
     external_links.push(uri)
     collection.push(uri)
+    return
   }
 
 }
