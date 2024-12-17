3ed68a66d4617c8d42651bea3f0ef2446ef98fe4
Abdelhak Marouane <am0089@uah.edu>
2024-12-17 13:05:10 -0600

    increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  timeout: 800000,
-  globalTimeout: 900000,
+  timeout: 80000000,
+  globalTimeout: 90000000,
   reporter: 'list',
   testDir: './link_checker_test',
   testMatch: ["**/*.js"]
